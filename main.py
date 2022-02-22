994eb0a112f8b2198539bbc3b0073f621a151b8d
Louis <perreaulou@gmail.com>
2022-02-22 15:41:32 +0100

    table profile added from json file

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import string
 import psycopg
 import json
 
-with psycopg.connect("dbname=doctolib_data user=louis") as db_conn:
+pg_conn_string = "dbname=doctolib_data user=louis"
+
+with psycopg.connect(pg_conn_string) as db_conn:
     print('connected to db')
 # Open a cursor to perform database operations
     with db_conn.cursor() as cur:
          cur.execute("""
-            DROP TABLE doctolib_centers;
+            DROP TABLE doctolib_centers_metadata;
          """)
          print('table dropped')
         
         # Execute a command: this creates a new table
     with db_conn.cursor() as cur:
         cur.execute("""
-            CREATE TABLE doctolib_centers (
+            CREATE TABLE doctolib_centers_metadata (
                 id serial PRIMARY KEY,
                 is_directory boolean,
                 address text,
@@ -35,7 +37,28 @@ with psycopg.connect("dbname=doctolib_data user=louis") as db_conn:
                 telehealth boolean
                 )
             """)
-    print('table created')
+    print('table metadata created')
+
+    with db_conn.cursor() as cur:
+         cur.execute("""
+            DROP TABLE doctolib_centers_profile;
+         """)
+         print('table dropped')
+        
+        # Execute a command: this creates a new table
+    with db_conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE doctolib_centers_profile (
+                id INTEGER,
+                name_with_title_and_determiner VARCHAR(128),
+                name_with_title VARCHAR(128),
+                speciality TEXT,
+                organization VARCHAR(64),
+                redirect_url VARCHAR(256),
+                language_list VARCHAR(128)
+                )
+            """)
+    print('table profile created')
 
 def read_json_file(file_path):
     file = open(file_path)
@@ -53,16 +76,32 @@ for file in files:
         data[json_file_path] = json_file
 
 def insert_metadata_in_db(metadata):
-    with psycopg.connect("dbname=doctolib_data user=louis") as db_conn:
+    with psycopg.connect(pg_conn_string) as db_conn:
         with db_conn.cursor() as cur:
             cur.execute("""
-            INSERT INTO doctolib_centers (id, is_directory, address, city, zipcode, link , cloudinary_public_id, profile_id , exact_match, priority_speciality, first_name, last_name, name_with_title , speciality, organization_status, place_id, telehealth)
+            INSERT INTO doctolib_centers_metadata (id, is_directory, address, city, zipcode, link , cloudinary_public_id, profile_id , exact_match, priority_speciality, first_name, last_name, name_with_title , speciality, organization_status, place_id, telehealth)
             VALUES (%(id)s, %(is_directory)s, %(address)s, %(city)s, %(zipcode)s, %(link)s,  %(cloudinary_public_id)s, %(profile_id)s,  %(exact_match)s, %(priority_speciality)s, %(first_name)s, %(last_name)s, %(name_with_title)s,  %(speciality)s, %(organization_status)s, %(place_id)s, %(telehealth)s);
             """,
             metadata)
 
+def insert_profile_in_db(profile):
+    with psycopg.connect(pg_conn_string) as db_conn:
+        with db_conn.cursor() as cur:
+            # we serialize `speciality` key, because it is a dict,
+            # and postgres will now expect a json string
+            profile['speciality'] = json.dumps(profile['speciality'])
+            cur.execute("""
+            INSERT INTO doctolib_centers_profile( id, name_with_title_and_determiner, name_with_title, speciality, organization, redirect_url, language_list)
+            VALUES ( %(id)s, %(name_with_title_and_determiner)s, %(name_with_title)s, %(speciality)s, %(organization)s, %(redirect_url)s, %(language_list)s);
+            """,
+            profile)
+
 for data_key, centers in data.items():
     for center in centers:
         metadata = center['metadata']
-        print(metadata)
-        insert_metadata_in_db(metadata)    
+        profile = center['data']['metadata']['profile']
+        insert_metadata_in_db(metadata)
+        print(profile)
+        insert_profile_in_db(profile)
+        
+        
