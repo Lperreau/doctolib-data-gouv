7dbb4d91ee1a598a7d61e33fb5efc5ef05a1b4eb
Louis <perreaulou@gmail.com>
2022-02-23 16:11:27 +0100

    handling csv files

--- main.py
+++ main.py
@@ -1,79 +1,102 @@
 import os
-import string
 import psycopg
 import json
+import csv
+import pandas
 
-pg_conn_string = "dbname=doctolib_data user=louis"
+SCRIPT_NAME = 'doctolib-data-gouv'
+PG_CONN_STRING = "dbname=doctolib_data user=louis"
+DATA_FOLDER = './raw_data'
 
-with psycopg.connect(pg_conn_string) as db_conn:
-    print('connected to db')
-# Open a cursor to perform database operations
-    with db_conn.cursor() as cur:
-         cur.execute("""
-            DROP TABLE doctolib_centers_metadata;
-         """)
-         print('table dropped')
-        
-        # Execute a command: this creates a new table
-    with db_conn.cursor() as cur:
-        cur.execute("""
-            CREATE TABLE doctolib_centers_metadata (
-                id serial PRIMARY KEY,
-                is_directory boolean,
-                address text,
-                city varchar(64),
-                zipcode varchar(8),
-                link varchar(256),
-                cloudinary_public_id varchar(36),
-                profile_id varchar(64),
-                exact_match boolean,
-                priority_speciality boolean,
-                first_name varchar(128),
-                last_name varchar(128),
-                name_with_title varchar(128),
-                speciality varchar(64),
-                organization_status varchar(64),
-                place_id varchar(64),
-                telehealth boolean
-                )
-            """)
-    print('table metadata created')
+# Tables creation
+def init_db_tables(pg_conn_string):
+    with psycopg.connect(pg_conn_string) as db_conn:
+        print('connected to db')
+        with db_conn.cursor() as cur:
+            cur.execute("""
+                DROP TABLE IF EXISTS public.src_stocks, doctolib_centers_metadata, doctolib_centers_profile;
+                """)
+            cur.execute("""
+                CREATE TABLE src_stocks (
+                    code_departement VARCHAR(8),
+                    departement VARCHAR(64),
+                    raison_sociale VARCHAR(128),
+                    libelle_pui VARCHAR(128),
+                    finess VARCHAR(16),
+                    type_de_vaccin VARCHAR(16),
+                    nb_ucd INTEGER,
+                    nb_doses INTEGER,
+                    date DATE)
+                """)
+            cur.execute("""
+                CREATE TABLE doctolib_centers_metadata (
+                    id serial PRIMARY KEY,
+                    is_directory boolean,
+                    address text,
+                    city varchar(64),
+                    zipcode varchar(8),
+                    link varchar(256),
+                    cloudinary_public_id varchar(36),
+                    profile_id varchar(64),
+                    exact_match boolean,
+                    priority_speciality boolean,
+                    first_name varchar(128),
+                    last_name varchar(128),
+                    name_with_title varchar(128),
+                    speciality varchar(64),
+                    organization_status varchar(64),
+                    place_id varchar(64),
+                    telehealth boolean
+                    )
+                """)
+            cur.execute("""
+                CREATE TABLE doctolib_centers_profile (
+                    id INTEGER,
+                    name_with_title_and_determiner VARCHAR(128),
+                    name_with_title VARCHAR(128),
+                    speciality TEXT,
+                    organization VARCHAR(64),
+                    redirect_url VARCHAR(256),
+                    language_list VARCHAR(128)
+                    )
+                """)
 
-    with db_conn.cursor() as cur:
-         cur.execute("""
-            DROP TABLE doctolib_centers_profile;
-         """)
-         print('table dropped')
-        
-        # Execute a command: this creates a new table
-    with db_conn.cursor() as cur:
-        cur.execute("""
-            CREATE TABLE doctolib_centers_profile (
-                id INTEGER,
-                name_with_title_and_determiner VARCHAR(128),
-                name_with_title VARCHAR(128),
-                speciality TEXT,
-                organization VARCHAR(64),
-                redirect_url VARCHAR(256),
-                language_list VARCHAR(128)
-                )
-            """)
-    print('table profile created')
+# Get data from raw data files - Json and CSV - and return python Dict
+def get_data(data_folder_path):
+    files = os.listdir(data_folder_path)
+    data = {}
+    for file in files:
+        file_path = "{}/{}".format(data_folder_path, file)
+        if file.endswith('.json'):
+            json_file = read_json_file(file_path)
+            data[file_path] = json_file
+        if file.endswith('.csv'):
+            csv_file = read_csv_file(file_path)
+            data[file_path] = csv_file
+    return data
 
 def read_json_file(file_path):
     file = open(file_path)
     data = json.load(file)
     return data
 
-data_folder = './raw data'
-files = os.listdir(data_folder)
-data = {}
-
-for file in files:
-    if file.endswith('.json'):
-        json_file_path = "{}/{}".format(data_folder, file)
-        json_file = read_json_file(json_file_path)
-        data[json_file_path] = json_file
+def read_csv_file(file_path, encoding = 'utf-8'):
+    with open(file_path, newline='', encoding=encoding) as csv_file:
+        try:
+            dialect = csv.Sniffer().sniff(csv_file.read(1024))
+            print(dialect)
+            csv_file.seek(0)
+            reader = csv.reader(csv_file, dialect)
+            print(file_path, reader)
+        except UnicodeDecodeError as e:
+            print(file_path, e)
+        # try:
+        #     for row in reader:
+        #         print(row)
+        # except csv.Error as e:
+        #     print('error reading csv file', e)
+        # except UnicodeDecodeError as e:
+        #     print('encoding error', e)
 
 def insert_metadata_in_db(metadata):
     with psycopg.connect(pg_conn_string) as db_conn:
@@ -96,12 +119,34 @@ def insert_profile_in_db(profile):
             """,
             profile)
 
-for data_key, centers in data.items():
-    for center in centers:
-        metadata = center['metadata']
-        profile = center['data']['metadata']['profile']
-        insert_metadata_in_db(metadata)
-        print(profile)
-        insert_profile_in_db(profile)
-        
-        
+def read_file_stocks_csv(file_path):
+    file = open(file_path)
+    data = csv.DictReader(file)
+    return data
+
+def insert_stocks_in_db(stocks):
+    with psycopg.connect(pg_conn_string) as db_conn:
+        with db_conn.cursor() as cur:
+            cur.execute('''
+            INSERT INTO src_stocks VALUE (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ''', stocks)
+            print('table stocks created')
+
+def insert_data_in_db():
+    for data_key, centers in data.items():
+        for center in centers:
+            metadata = center['metadata']
+            profile = center['data']['metadata']['profile']
+            insert_metadata_in_db(metadata)
+            insert_profile_in_db(profile)
+
+
+
+def main(name, pg_conn, data_folder):
+    print(name, "is running")
+    init_db_tables(pg_conn)
+    print('all tables created') 
+    data = get_data(data_folder)
+    print('get data successful')
+
+main(SCRIPT_NAME, PG_CONN_STRING, DATA_FOLDER)
--- readme.md
+++ readme.md
@@ -19,3 +19,7 @@ Run command `pip install -r requirements.txt`
  1. Make sure the user name is the same as your linux user name
  1. If you don't you might get the following error: "psycopg.OperationalError: connection failed: FATAL:  Peer authentication failed for user "robot" "
 3. Run `grant all privileges on database doctolib_data to robot;`
+
+## Find CSVs files encoding
+1. run command `file -bi raw_data/allocations-vs-rdv.csv `
+ . In our case, allocations-vs-rdv has encoding `charset=unknown-8bit` which breaks our function reader to read the csv.
